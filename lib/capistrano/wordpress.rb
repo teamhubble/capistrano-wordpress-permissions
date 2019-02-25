7c90fd58139996ea212581efc728a768108eb6d4
alexzicat <dev.alexzicat@gmail.com>
2019-02-25 09:53:07 -0500

    initial commit

---
+++ lib/capistrano/wordpress.rb
@@ -0,0 +1,8 @@
+require "capistrano/wordpress/version"
+
+module Capistrano
+  module Wordpress
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/capistrano/wordpress/version.rb
@@ -0,0 +1,5 @@
+module Capistrano
+  module Wordpress
+    VERSION = "0.1.0"
+  end
+end
